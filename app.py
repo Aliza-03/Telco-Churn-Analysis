5cbea37cfa0379e3a4ada9ae7cc7d44599d61d06
Aliza-03 <alizaibm3000@gmail.com>
2024-11-21 12:57:50 +0500

    add dataset

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
@@ -6,5 +6,18 @@ app = Flask(__name__)
 def home():
     return "Hello, Flask! Welcome to your first app."
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/insights')
+def insights():
+    return render_template('insights.html')
+
+@app.route('/model')
+def model():
+    return render_template('model.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
+
